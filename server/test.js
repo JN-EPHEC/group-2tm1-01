5f54d08513156747dc899d295153cf61b970d89a
4B-Cycle <b.eloin@students.ephec.be>
2026-05-28 18:20:49 +0200

    #66 fix github action supression fichier de test

--- server/test.js
+++
@@ -1,3 +0,0 @@
-import { supabase } from './src/config/supabase';
-supabase.from('orders').select('*, order_items(*, products(*))').then(r => console.log(JSON.stringify(r.data, null, 2)));
-//# sourceMappingURL=test.js.map
--- server/test.ts
+++
@@ -1,5 +0,0 @@
-import { supabase } from "./src/config/supabase";
-supabase
-  .from("orders")
-  .select("*, order_items(*, products(*))")
-  .then((r) => console.log(JSON.stringify(r.data, null, 2)));
